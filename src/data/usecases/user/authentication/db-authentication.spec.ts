9330da549000206d1b600b991f4c1ea6381590dc
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-06 17:30:34 -0300

    test: DbAuthentication returns null if LoadUserByEmail returns null

--- src/data/usecases/user/authentication/db-authentication.spec.ts
+++ src/data/usecases/user/authentication/db-authentication.spec.ts
@@ -32,4 +32,10 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(mockAuthenticationParams())
     await expect(promise).rejects.toThrow()
   })
+  test('Should return null if LoadUserByEmailRepository returns null', async () => {
+    const { sut, loadUserByEmailRepositoryStub } = mockSut()
+    jest.spyOn(loadUserByEmailRepositoryStub, 'loadByEmail').mockResolvedValue(null)
+    const accessToken = await sut.auth(mockAuthenticationParams())
+    expect(accessToken).toBe(null)
+  })
 })
