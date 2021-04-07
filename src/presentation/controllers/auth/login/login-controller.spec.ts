391b13463e4dec347c777d9d54c78e690924bdf5
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-07 00:37:29 -0300

    feat: add LoginController

---
+++ src/presentation/controllers/auth/login/login-controller.spec.ts
@@ -0,0 +1,97 @@
+import { badRequest, ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper'
+import { LoginController } from './login-controller'
+import { Validation } from '@/presentation/protocols/validation'
+import { EmailValidator } from '@/validation/protocols/email-validator'
+import { Authentication } from '@/domain/usecases/user/authentication'
+import { HttpRequest } from '@/presentation/protocols'
+import { mockEmailValidator } from '@/validation/test'
+import { mockAuthentication } from '@/presentation/test/mock-authentication'
+import { mockValidation } from '@/presentation/test/mock-validation'
+import { mockUserModel } from '@/domain/test'
+
+type SutTypes = {
+  sut: LoginController
+  emailValidator: EmailValidator
+  authentication: Authentication
+  validationStub: Validation
+}
+
+const mockRequest = (): HttpRequest => ({
+  body: {
+    email: 'any_email@mail.com',
+    password: 'any_password'
+  }
+})
+
+const mockSut = (): SutTypes => {
+  const emailValidator = mockEmailValidator()
+  const authentication = mockAuthentication()
+  const validationStub = mockValidation()
+  return {
+    sut: new LoginController(authentication, validationStub),
+    emailValidator,
+    authentication,
+    validationStub
+  }
+}
+
+describe('Login Controlle', () => {
+  test('Should call Authentication with correct values', async () => {
+    const { sut, authentication } = mockSut()
+
+    const authSpy = jest.spyOn(authentication, 'auth')
+
+    await sut.handle(mockRequest())
+
+    expect(authSpy).toHaveBeenCalledWith({
+      email: mockRequest().body.email,
+      password: mockRequest().body.password
+    })
+  })
+  test('Should return 401 invalid credentials are provided', async () => {
+    const { sut, authentication } = mockSut()
+
+    jest.spyOn(authentication, 'auth').mockReturnValueOnce(Promise.resolve(null))
+
+    const response = await sut.handle(mockRequest())
+
+    expect(response).toEqual(unauthorized())
+  })
+
+  test('Should return 500 if Authentication Throws', async () => {
+    const { sut, authentication } = mockSut()
+
+    jest.spyOn(authentication, 'auth').mockReturnValueOnce(Promise.reject(new Error()))
+
+    const response = await sut.handle(mockRequest())
+
+    expect(response).toEqual(serverError(new Error()))
+  })
+  test('Should return 200 valid credentials are provided', async () => {
+    const { sut } = mockSut()
+    const response = await sut.handle(mockRequest())
+
+    expect(response).toEqual(ok({
+      token: 'any_token',
+      user: mockUserModel()
+    }))
+  })
+  test('Should call Validation with correct values', async () => {
+    const { sut, validationStub } = mockSut()
+
+    const validateSpy = jest.spyOn(validationStub, 'validate')
+
+    const httpRequest = mockRequest()
+
+    await sut.handle(httpRequest)
+    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+  })
+  test('Should return 400 if validation returns an error', async () => {
+    const { sut, validationStub } = mockSut()
+
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
+    const httpRequest = mockRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+})
---
+++ src/presentation/controllers/auth/login/login-controller.ts
@@ -0,0 +1,29 @@
+import { Authentication } from '@/domain/usecases/user/authentication'
+import { badRequest, ok, serverError, unauthorized } from '@/presentation/helpers/http/http-helper'
+import { Controller, HttpRequest, HttpResponse } from '@/presentation/protocols'
+import { Validation } from '@/presentation/protocols/validation'
+
+export class LoginController implements Controller {
+  constructor (
+    private readonly authentication: Authentication,
+    private readonly validation: Validation
+  ) { }
+
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const error = this.validation.validate(httpRequest.body)
+
+      if (error) return badRequest(error)
+
+      const { email, password } = httpRequest.body
+
+      const token = await this.authentication.auth({ email, password })
+
+      if (!token) return unauthorized()
+
+      return ok(token)
+    } catch (error) {
+      return serverError(error)
+    }
+  }
+}
