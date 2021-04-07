7cd76518759aef39627c0b8a7d872cdb7b7696eb
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-07 18:23:31 -0300

    feat: added AddMovementController factory

---
+++ src/main/factories/usecases/movement/add-movement/add-movement-factory.ts
@@ -0,0 +1,12 @@
+import { DbAddMovement } from '@/data/usecases/movement/add-movement/db-add-movement'
+import { AddMovement } from '@/domain/usecases/movement/add-movement'
+import { AccountMongoRepository } from '@/infra/db/mongodb/account/account-mongo-repository'
+import { MovementMongoRepository } from '@/infra/db/mongodb/movement/movement-mongo-repository'
+import { MovementTypeMongoRepository } from '@/infra/db/mongodb/movementType/movement-type-mongo-repository'
+
+export const makeDbAddMovement = (): AddMovement => {
+  const movementMongoRepository = new MovementMongoRepository()
+  const accountMongoRepository = new AccountMongoRepository()
+  const movementTypeMongoRepository = new MovementTypeMongoRepository()
+  return new DbAddMovement(movementMongoRepository, accountMongoRepository, movementTypeMongoRepository)
+}
