5da7829ecfda0627862d3b26da21176f7e8766c5
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-07 16:27:04 -0300

    feat: ensure GetAccountMongoRepository returns an account

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -2,6 +2,7 @@ import { Collection } from 'mongodb'
 import MockDate from 'mockdate'
 import { mongoHelper } from '../helpers/mongo-helper'
 import { AccountMongoRepository } from './account-mongo-repository'
+import { mockAccountModel } from '@/domain/test/mock-account'
 
 type SutTypes = {
   sut: AccountMongoRepository
@@ -31,14 +32,35 @@ describe('AccountMongoRepository', () => {
     accountCollection = await mongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Should add an account on database', async () => {
-    const { sut } = mockSut()
-    await sut.add('any_id', 0)
-    const account = await accountCollection.findOne({ userId: 'any_id' })
-    expect(account).toBeTruthy()
-    expect(account?._id).toBeTruthy()
-    expect(account?.balance).toBe(0)
-    expect(account?.createdAt).toEqual(new Date())
-    expect(account?.updatedAt).toEqual(new Date())
+  describe('add()', () => {
+    test('Should add an account on database', async () => {
+      const { sut } = mockSut()
+      await sut.add('any_id', 0)
+      const account = await accountCollection.findOne({ userId: 'any_id' })
+      expect(account).toBeTruthy()
+      expect(account?._id).toBeTruthy()
+      expect(account?.balance).toBe(0)
+      expect(account?.createdAt).toEqual(new Date())
+      expect(account?.updatedAt).toEqual(new Date())
+    })
+  })
+  describe('getById()', () => {
+    test('Should returns an account ', async () => {
+      const { sut } = mockSut()
+      const result = await accountCollection.insertOne({
+        balance: mockAccountModel().balance,
+        createdAt: mockAccountModel().createdAt,
+        updatedAt: mockAccountModel().updatedAt,
+        userId: mockAccountModel().userId
+      })
+      const id = mongoHelper.map(result.ops[0]).id
+      const account = await sut.getById(id)
+      expect(account).toBeTruthy()
+      expect(account?.id).toBeTruthy()
+      expect(account?.balance).toBe(mockAccountModel().balance)
+      expect(account?.createdAt).toEqual(mockAccountModel().createdAt)
+      expect(account?.updatedAt).toEqual(mockAccountModel().updatedAt)
+      expect(account?.userId).toEqual(mockAccountModel().userId)
+    })
   })
 })
--- src/infra/db/mongodb/account/account-mongo-repository.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.ts
@@ -1,7 +1,10 @@
 import { AddAccountRepository } from '@/data/protocols/db/account/add-account-repository'
+import { GetAccountRepository } from '@/data/protocols/db/account/get-account-repository'
+import { AccountModel } from '@/domain/models/account'
+import { ObjectId } from 'bson'
 import { mongoHelper } from '../helpers/mongo-helper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, GetAccountRepository {
   async add (userId: string, balance: number): Promise<void> {
     const accountCollection = await mongoHelper.getCollection('accounts')
 
@@ -9,5 +12,11 @@ export class AccountMongoRepository implements AddAccountRepository {
     const updatedAt = new Date()
 
     await accountCollection.insertOne({ userId, balance, createdAt, updatedAt })
+  }
+
+  async getById (id: string): Promise<AccountModel | null> {
+    const accountCollection = await mongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ _id: new ObjectId(id) })
+    return account && mongoHelper.map(account)
   }
 }
