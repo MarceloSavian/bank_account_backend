242aa3b6a5b93d080cf8ef5de64c2c7b5e8a04e9
Marcelo Savian <marcelo.savian@gmail.com>
2021-04-08 16:46:02 -0300

    feat: ensure DbGetAccountByUserId returns an account on success

--- src/data/usecases/account/get-account-by-user-id/db-get-account-by-user-id.spec.ts
+++ src/data/usecases/account/get-account-by-user-id/db-get-account-by-user-id.spec.ts
@@ -1,6 +1,8 @@
 import { GetAccountByUserIdRepository } from '@/data/protocols/db/account/get-account-by-user-id-repository'
 import { mockGetAccountByUserIdRepository } from '@/data/test/mock-db-account'
+import { mockAccountModel } from '@/domain/test/mock-account'
 import { DbGetAccountByUserId } from './db-get-account-by-user-id'
+import MockDate from 'mockdate'
 
 type SutTypes = {
   sut: DbGetAccountByUserId
@@ -17,10 +19,21 @@ const mockSut = (): SutTypes => {
 }
 
 describe('DbGetAccountByUserId', () => {
+  beforeAll(() => {
+    MockDate.set(new Date())
+  })
+  afterAll(() => {
+    MockDate.reset()
+  })
   test('should calls GetAccountByUserIdRepository with correct values', async () => {
     const { sut, getAccountByUserIdRepositoryStub } = mockSut()
     const getSpy = jest.spyOn(getAccountByUserIdRepositoryStub, 'getByUserId')
     await sut.get('any_id')
     expect(getSpy).toHaveBeenCalledWith('any_id')
+  })
+  test('should returns an account on succes', async () => {
+    const { sut } = mockSut()
+    const account = await sut.get('any_id')
+    expect(account).toEqual(mockAccountModel())
   })
 })
